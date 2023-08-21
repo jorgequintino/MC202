c5dd2d1604b6abb403c6023bd79bb27d36f28d18
haitsr <jorgefelipe11_@hotmail.com>
2023-08-21 17:32:41 -0300

    ju

--- lab03/golfe.c
+++ lab03/golfe.c
@@ -1,3 +1,11 @@
+# include <stdio.h>
+
 int main(){
-    
+    int games, players, circuits, equipaments;
+    scanf("%f ", games) ;
+    scanf("%f %f %f ", players, circuits, equipaments) ;
+    // para cada circuits tem players + 1 dados
+    // (numero circuito, jogada dos jogadores)
+
+    float price = 10*circuits + 17.5*equipaments ;
 }
